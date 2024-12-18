ae36c29c1d5aad0df84db632340c336f19b56c11
GodMinseok <mseok830@naver.com>
2024-12-18 21:00:33 +0900

    home, GlobalStyle추가

--- baseball_test/src/App.tsx
+++ baseball_test/src/App.tsx
@@ -1,5 +1,17 @@
+import { createGlobalStyle } from "styled-components";
 import "./App.css";
 
 function App() {}
 
 export default App;
+
+// 공통적으로 전역에서 사용할 Global CSS
+const GlobalStyle = createGlobalStyle`
+  ${onreset}
+  body{
+    background-color: red;
+    color: white;
+    font-family: system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif
+  }
+
+`;
