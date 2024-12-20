e2f6fba9403b745ee36db64c8acab2c0a4b23dc8
GodMinseok <mseok830@naver.com>
2024-12-20 15:56:39 +0900

    홈화면 스타일 적용 및 module 재 설치

--- baseball_test/src/screens/home.tsx
+++ baseball_test/src/screens/home.tsx
@@ -1,10 +1,27 @@
 import React from "react";
+// import { useNavigate } from "react-router-dom";
+// import styled from "styled-components";
+
+// const Container = styled.div``;
+// const Title = styled.div``;
+// const StartButton = styled.div``;
+
+// const Home = () => {
+//   const navigate = useNavigate();
+
+//   return (
+//     <Container>
+//       <Title>야구 관람 유형 테스트</Title>
+//       <StartButton onClick={() => navigate("/test")}>시작하기</StartButton>
+//     </Container>
+//   );
+// };
+
+// export default Home;
+
+// Home 컴포넌트
 import { useNavigate } from "react-router-dom";
 import styled from "styled-components";
-
-const Container = styled.div``;
-const Title = styled.div``;
-const StartButton = styled.div``;
 
 const Home = () => {
   const navigate = useNavigate();
@@ -12,9 +29,39 @@ const Home = () => {
   return (
     <Container>
       <Title>야구 관람 유형 테스트</Title>
-      <StartButton onClick={() => navigate("/test")}>시작하기</StartButton>
+      <StartButton onClick={() => navigate("/select-seats")}>
+        시작하기
+      </StartButton>
     </Container>
   );
 };
 
 export default Home;
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  height: 100vh;
+`;
+
+const Title = styled.h1`
+  font-size: 2rem;
+  color: #005662;
+  margin-bottom: 20px;
+`;
+
+const StartButton = styled.button`
+  padding: 12px 24px;
+  font-size: 18px;
+  background-color: #1379ff;
+  color: white;
+  border: none;
+  border-radius: 8px;
+  cursor: pointer;
+
+  &:hover {
+    background-color: #0e5bcc;
+  }
+`;
