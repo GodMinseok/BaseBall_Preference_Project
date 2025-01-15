e6f3d836edece3816013bead4251a5d3c15d023c
GodMinseok <mseok830@naver.com>
2025-01-15 23:41:39 +0900

    코드 간결화

--- baseball_test/src/screens/home.tsx
+++ baseball_test/src/screens/home.tsx
@@ -1,25 +1,4 @@
 import React from "react";
-// import { useNavigate } from "react-router-dom";
-// import styled from "styled-components";
-
-// const Container = styled.div``;
-// const Title = styled.div``;
-// const StartButton = styled.div``;
-
-// const Home = () => {
-//   const navigate = useNavigate();
-
-//   return (
-//     <Container>
-//       <Title>야구 관람 유형 테스트</Title>
-//       <StartButton onClick={() => navigate("/test")}>시작하기</StartButton>
-//     </Container>
-//   );
-// };
-
-// export default Home;
-
-// Home 컴포넌트
 import { useNavigate } from "react-router-dom";
 import styled from "styled-components";
 
